5a004489f17b784903b28b1d939080c6dd5853a5
Leooman <yonghenglion@gmail.com>
2018-08-16 17:04:28 +0800

    添加打包路径

--- src/config/env.js
+++ src/config/env.js
@@ -12,8 +12,7 @@ if (process.env.NODE_ENV == 'development') {
 	if(process.env.TYPE == 'local') baseUrl = 'http://localhost:3000'
 	if(process.env.TYPE == 'online') baseUrl = 'http://localhost:8080'
 }else{
-	// baseUrl = (process.env.HOST == 'vadmin') ? '/Vadmin' : ''
-	baseUrl = ''
+	baseUrl = (process.env.HOST == 'vadmin') ? '/Vadmin' : ''
 }
 
 export {
