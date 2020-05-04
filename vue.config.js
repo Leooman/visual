5503aad26cd47b28307f68b2c35c5db9bc97deff
Leooman <351234955@qq.com>
2020-05-04 11:36:53 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ function resolve(dir) {
 const port = 8081
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/micro-frontends/vue/',
-  outputDir: '../main-nuxt/dist/vue',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/Vadmin',
+  outputDir: 'dist',
   assetsDir: 'static',
   filenameHashing: true,
   devServer: {
